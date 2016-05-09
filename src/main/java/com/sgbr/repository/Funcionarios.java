2da0ab50e6ed0e43e1065cc5e856ade2c8145a61
o.junior <o.junior@Estacao59.GFA.local>
2016-05-09 16:35:29 -0300

    commit

--- src/main/java/com/sgbr/repository/Funcionarios.java
+++ src/main/java/com/sgbr/repository/Funcionarios.java
@@ -5,20 +5,8 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.PersistenceException;
-
-import org.apache.commons.lang3.StringUtils;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.criterion.MatchMode;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Restrictions;
 
 import com.sgbr.model.Funcionario;
-import com.sgbr.repository.filter.FuncionarioFilter;
-import com.sgbr.service.NegocioException;
-import com.sgbr.util.jpa.Transactional;
 
 public class Funcionarios implements Serializable {
 
@@ -27,54 +15,14 @@ public class Funcionarios implements Serializable {
 	@Inject
 	private EntityManager manager;
 
-	public Funcionario guardar(Funcionario funcionario) {
-		return manager.merge(funcionario);
-	}
-	
-	@Transactional
-	public void remover(Funcionario funcionario) {
-		try {
-			funcionario = porIdFuncionario(funcionario.getIdFuncionario());
-			manager.remove(funcionario);
-			manager.flush();
-		} catch (PersistenceException e) {
-			throw new NegocioException("Funcionario não pode ser excluído.");
-		}
-	}
-
-	public Funcionario porIdFuncionario(String idFuncionario) {
-		try {
-			return manager.createQuery("from Funcionario where upper(idFuncionario) = :idFuncionario", Funcionario.class)
-				.setParameter("idFuncionario", idFuncionario.toUpperCase())
-				.getSingleResult();
-		} catch (NoResultException e) {
-			return null;
-		}
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<Funcionario> filtrados(FuncionarioFilter filtro) {
-		Session session = manager.unwrap(Session.class);
-		Criteria criteria = session.createCriteria(Funcionario.class);
-		
-		if (StringUtils.isNotBlank(filtro.getIdFuncionario())) {
-			criteria.add(Restrictions.eq("idFuncionario", filtro.getIdFuncionario()));
-		}
-		
-		if (StringUtils.isNotBlank(filtro.getDescricao())) {
-			criteria.add(Restrictions.ilike("descricao", filtro.getDescricao(), MatchMode.ANYWHERE));
-		}
-		
-		return criteria.addOrder(Order.asc("descricao")).list();
-	}
-
-	public Funcionario porIdFuncionario(Long idFuncionario) {
-		return manager.find(Funcionario.class, idFuncionario);
+	public Funcionario porId(Long id) {
+		return this.manager.find(Funcionario.class, id);
 	}
 
 	public List<Funcionario> porNome(String nome) {
-		return this.manager.createQuery("from Funcionario where upper(descricao) like :descricao", Funcionario.class)
-				.setParameter("descricao", nome.toUpperCase() + "%").getResultList();
+		return this.manager
+				.createQuery("from Funcionario where upper(nome) like :nome",
+						Funcionario.class)
+				.setParameter("nome", nome.toUpperCase() + "%").getResultList();
 	}
-	
 }
