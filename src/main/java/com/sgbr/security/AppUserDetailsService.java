aa68098277b578675bf5d3ad9a9e30ed2dcbb9d3
o.junior <osmarcandido.r@gmail.com>
2016-05-11 08:30:52 -0300

    Controle de acesso 1.2

---
+++ src/main/java/com/sgbr/security/AppUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.sgbr.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.sgbr.model.Funcionario;
+import com.sgbr.model.Perfil;
+import com.sgbr.repository.Funcionarios;
+import com.sgbr.util.cdi.CDIServiceLocator;
+
+public class AppUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+		Funcionarios funcionarios = CDIServiceLocator.getBean(Funcionarios.class);
+		Funcionario funcionario = funcionarios.porLogin(login);
+		FuncionarioSistema user = null;
+
+		if (funcionario != null) {
+			user = new FuncionarioSistema(funcionario, getPerfis(funcionario));
+		}
+
+		return user;
+	}
+
+	private Collection<? extends GrantedAuthority> getPerfis(Funcionario funcionario) {
+		List<SimpleGrantedAuthority> perfis = new ArrayList<>();
+		// TODO Auto-generated method stub
+		return perfis;
+	}
+
+}
---
+++ src/main/java/com/sgbr/security/FuncionarioSistema.java
@@ -0,0 +1,25 @@
+package com.sgbr.security;
+
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import com.sgbr.model.Funcionario;
+
+
+public class FuncionarioSistema extends User{
+
+	private static final long serialVersionUID=1L;
+	
+	private Funcionario funcionario;
+	
+	public FuncionarioSistema(Funcionario funcionario, Collection<? extends GrantedAuthority> authorities) {
+		super(funcionario.getLogin(), funcionario.getSenha(), authorities);
+		this.funcionario = funcionario;
+		// TODO Auto-generated constructor stub	
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+}
