56d56e43cbcb45cd6f83deb8467e205a898de2d9
o.junior <osmarcandido.r@gmail.com>
2016-06-03 12:56:18 -0300

    Produtos mais vendidos

--- src/main/java/com/sgbr/controller/RelatorioProdutosMaisVendidosBean.java
+++ src/main/java/com/sgbr/controller/RelatorioProdutosMaisVendidosBean.java
@@ -51,6 +51,7 @@ public class RelatorioProdutosMaisVendidosBean implements Serializable {
 		Session session = manager.unwrap(Session.class);
 		session.doWork(executor);
 
+		
 		facesContext.responseComplete();
 	}
 	
