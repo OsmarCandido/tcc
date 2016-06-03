6ad93302175252bfc5d30c66a60351e7060383f5
o.junior <osmarcandido.r@gmail.com>
2016-06-03 14:42:40 -0300

    Produtos a comprar

--- src/main/java/com/sgbr/controller/RelatorioProdutosAComprarBean.java
+++ src/main/java/com/sgbr/controller/RelatorioProdutosAComprarBean.java
@@ -1,7 +1,6 @@
 package com.sgbr.controller;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,7 +10,6 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.constraints.NotNull;
 
 import org.hibernate.Session;
 
@@ -20,13 +18,13 @@ import com.sgbr.util.report.ExecutorRelatorio;
 
 @Named
 @ViewScoped
-public class RelatorioProdutosMaisVendidosBean implements Serializable {
+public class RelatorioProdutosAComprarBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 
-	private Date dataInicio;
-	private Date dataFim;
+//	private Date dataInicio;
+//	private Date dataFim;
 
 	@Inject
 	private FacesContext facesContext;
@@ -40,12 +38,10 @@ public class RelatorioProdutosMaisVendidosBean implements Serializable {
 	public void emitir(){
 		
 		Map<String, Object> parametros = new HashMap<>();
-		parametros.put("data_inicio", this.dataInicio);
-		parametros.put("data_fim", this.dataFim);
-			
-		
-		ExecutorRelatorio executor = new ExecutorRelatorio("/relatorios/Produtosmaisvendidos.jasper",
-				this.response, parametros, "Produtos Mais Vendidos.pdf");
+//		parametros.put("data_inicio", this.dataInicio);
+//		parametros.put("data_fim", this.dataFim);
+		ExecutorRelatorio executor = new ExecutorRelatorio("/relatorios/ProdutosAcomprar.jasper",
+				this.response, parametros, "Produtos a Comprar.pdf");
 		
 
 		Session session = manager.unwrap(Session.class);
@@ -54,22 +50,22 @@ public class RelatorioProdutosMaisVendidosBean implements Serializable {
 		facesContext.responseComplete();
 	}
 	
-	@NotNull
-	public Date getDataInicio() {
-		return dataInicio;
-	}
+//	@NotNull
+//	public Date getDataInicio() {
+//		return dataInicio;
+//	}
 
-	public void setDataInicio(Date dataInicio) {
-		this.dataInicio = dataInicio;
-	}
+//	public void setDataInicio(Date dataInicio) {
+//		this.dataInicio = dataInicio;
+//	}
 
-	@NotNull
-	public Date getDataFim() {
-		return dataFim;
-	}
-
-	public void setDataFim(Date dataFim) {
-		this.dataFim = dataFim;
-	}
+//	@NotNull
+//	public Date getDataFim() {
+//		return dataFim;
+//	}
+//
+//	public void setDataFim(Date dataFim) {
+//		this.dataFim = dataFim;
+//	}
 
 }
